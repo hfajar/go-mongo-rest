33b4ddfcb0517aa7a81f803e07162053034e6097
Fajar Hidayatullah <hidayatullah.fajar@gmail.com>
2018-11-06 17:57:14 +0700

    ubah port connection

--- services/database.go
+++ services/database.go
@@ -4,7 +4,7 @@ import "gopkg.in/mgo.v2"
 
 // Connect is for connection database
 func Connect() (*mgo.Session, error) {
-	var session, err = mgo.Dial("localhost")
+	var session, err = mgo.Dial("localhost:27018")
 	if err != nil {
 		return nil, err
 	}
